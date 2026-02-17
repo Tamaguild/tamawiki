188a449f8ec0cc74a2858b029c6e54bb1bd82efe
Duong109 <thuy1984ktcn@gmail.com>
2026-02-17 20:34:55 +0700

    MAP UPDATE 👍

---
+++ map/LayerPages/Layer1/data.js
@@ -0,0 +1,24 @@
+export const DATA = {
+  basic: {
+    title: "Basic",
+    content: "Layer 1 is the first layer you will encounter in the game. The layer itself still pretty safe, but there still some monster can pose a great threat to newcomer, especially those dont have any gears yet."
+  },
+
+  mustVisit: [
+    {
+      id: 1,
+      name: "Peach Blossom Village",
+      location: "X: Y: Z:",
+      danger: "Low",
+      note: "I considered this is the best place to hunt skelly. The terrain is easy to navigate and spot those skelly"
+    },
+  ],
+
+  tips: [
+    {
+      id: 1,
+      title: "Tip 1: Running :tuh:",
+      content: "Always try to avoid any fight at first, you can easily be one-tap by some random bird :mogicrazy:."
+    }
+  ]
+};
