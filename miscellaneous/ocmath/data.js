a5fc1480c7df8ba19671abba91f06a0114d78c5f
Duong109 <thuy1984ktcn@gmail.com>
2026-05-10 11:03:51 +0700

    add orth coin caculator + miscellanous page

---
+++ miscellaneous/ocmath/data.js
@@ -0,0 +1,326 @@
+export const PRODUCTS = [
+  {
+    name: "Orb Piercer Spike",
+    amount: 7,
+    img: "../../avatar/block_oc.png",
+    desc: ""
+  },
+  {
+    name: "Orb Piercer Face Fragments",
+    amount: 4,
+    img: "../../avatar/block_oc.png",
+    desc: ""
+  },
+  {
+    name: "Splitjaw Scales",
+    amount: 8,
+    img: "../../avatar/block_oc.png",
+    desc: ""
+  },
+  {
+    name: "Splitjaw Gunk",
+    amount: 9,
+    img: "../../avatar/block_oc.png",
+    desc: ""
+  },
+  {
+    name: "Tesuchi Scales",
+    amount: 6,
+    img: "../../avatar/block_oc.png",
+    desc: ""
+  },
+  {
+    name: "Silkfang Eye",
+    amount: 8,
+    img: "../../avatar/block_oc.png",
+    desc: ""
+  },
+  {
+    name: "Silkfang Silk",
+    amount: 9,
+    img: "../../avatar/block_oc.png",
+    desc: ""
+  },
+  {
+    name: "Cyatoria Feather",
+    amount: 10,
+    img: "../../avatar/block_oc.png",
+    desc: ""
+  },
+  {
+    name: "Rohana Tail",
+    amount: 10,
+    img: "../../avatar/block_oc.png",
+    desc: ""
+  },
+  {
+    name: "Dosetori Wing",
+    amount: 12,
+    img: "../../avatar/block_oc.png",
+    desc: ""
+  },
+  {
+    name: "Dosetori Bone",
+    amount: 10,
+    img: "../../avatar/block_oc.png",
+    desc: ""
+  },
+  {
+    name: "Corpse Weeper Eye",
+    amount: 8,
+    img: "../../avatar/block_oc.png",
+    desc: ""
+  },
+  {
+    name: "Corpse Weeper Feather",
+    amount: 10,
+    img: "../../avatar/block_oc.png",
+    desc: ""
+  },
+  {
+    name: "Hammerbeak Beak",
+    amount: 8,
+    img: "../../avatar/block_oc.png",
+    desc: ""
+  },
+  {
+    name: "Hammerbeak Feather",
+    amount: 10,
+    img: "../../avatar/block_oc.png",
+    desc: ""
+  },
+  {
+    name: "Onitsuchi Skull",
+    amount: 9,
+    img: "../../avatar/block_oc.png",
+    desc: ""
+  },
+  {
+    name: "Onitsuchi Feather",
+    amount: 10,
+    img: "../../avatar/block_oc.png",
+    desc: ""
+  },
+  {
+    name: "Kuongatari Abdomen",
+    amount: 8,
+    img: "../../avatar/block_oc.png",
+    desc: ""
+  },
+  {
+    name: "Cooked Kuongatari Abdomen",
+    amount: 8,
+    img: "../../avatar/block_oc.png",
+    desc: ""
+  },
+  {
+    name: "Kuongatari Juice",
+    amount: 8,
+    img: "../../avatar/block_oc.png",
+    desc: ""
+  },
+  {
+    name: "Madokajack Scales",
+    amount: 12,
+    img: "../../avatar/block_oc.png",
+    desc: ""
+  },
+  {
+    name: "Madokajack Bone",
+    amount: 11,
+    img: "../../avatar/block_oc.png",
+    desc: ""
+  },
+  {
+    name: "Madokajack Poop",
+    amount: 10,
+    img: "../../avatar/block_oc.png",
+    desc: ""
+  },
+  {
+    name: "Parasitic Watershrooms",
+    amount: 10,
+    img: "../../avatar/block_oc.png",
+    desc: ""
+  },
+  {
+    name: "Chimokami Tail",
+    amount: 6,
+    img: "../../avatar/block_oc.png",
+    desc: ""
+  },
+  {
+    name: "Chimokami Fang",
+    amount: 6,
+    img: "../../avatar/block_oc.png",
+    desc: ""
+  },
+  {
+    name: "Asmite Antler",
+    amount: 8,
+    img: "../../avatar/block_oc.png",
+    desc: ""
+  },
+  {
+    name: "Ashmite Hoof",
+    amount: 9,
+    img: "../../avatar/block_oc.png",
+    desc: ""
+  },
+  {
+    name: "Hashofu Body",
+    amount: 7,
+    img: "../../avatar/block_oc.png",
+    desc: ""
+  },
+  {
+    name: "Hashofu Eye",
+    amount: 7,
+    img: "../../avatar/block_oc.png",
+    desc: ""
+  },
+  {
+    name: "Abyssal Snail Gunk",
+    amount: 7,
+    img: "../../avatar/block_oc.png",
+    desc: ""
+  },
+  {
+    name: "Abyssal Snail Shell",
+    amount: 7,
+    img: "../../avatar/block_oc.png",
+    desc: ""
+  },
+  {
+    name: "Kakatsumuri Shell",
+    amount: 6,
+    img: "../../avatar/block_oc.png",
+    desc: ""
+  },
+  {
+    name: "Kagi Tachikiri Pincer",
+    amount: 6,
+    img: "../../avatar/block_oc.png",
+    desc: ""
+  },
+  {
+    name: "Titanjaw Pearl",
+    amount: 5,
+    img: "../../avatar/block_oc.png",
+    desc: ""
+  },
+  {
+    name: "Titanjaw Scales",
+    amount: 7,
+    img: "../../avatar/block_oc.png",
+    desc: ""
+  },
+  {
+    name: "Makihige Tentacle",
+    amount: 9,
+    img: "../../avatar/block_oc.png",
+    desc: ""
+  },
+  {
+    name: "Makihige Ink Sac",
+    amount: 9,
+    img: "../../avatar/block_oc.png",
+    desc: ""
+  },
+  {
+    name: "Kazura Squid Ink Sac",
+    amount: 10,
+    img: "../../avatar/block_oc.png",
+    desc: ""
+  },
+  {
+    name: "Sakuranbo Dust",
+    amount: 5,
+    img: "../../avatar/block_oc.png",
+    desc: ""
+  },
+  {
+    name: "Mantoyer Horns",
+    amount: 10,
+    img: "../../avatar/block_oc.png",
+    desc: ""
+  },
+  {
+    name: "Woodcrier Horn",
+    amount: 10,
+    img: "../../avatar/block_oc.png",
+    desc: ""
+  },
+  {
+    name: "Rockwalker Mandible",
+    amount: 14,
+    img: "../../avatar/block_oc.png",
+    desc: ""
+  },
+  {
+    name: "Hisoshi Claw",
+    amount: 16,
+    img: "../../avatar/block_oc.png",
+    desc: ""
+  },
+  {
+    name: "Buroguro Stinger",
+    amount: 10,
+    img: "../../avatar/block_oc.png",
+    desc: ""
+  },
+  {
+    name: "Daikonchu Carapace",
+    amount: 9,
+    img: "../../avatar/block_oc.png",
+    desc: ""
+  },
+  {
+    name: "Stinger Horn",
+    amount: 9,
+    img: "../../avatar/block_oc.png",
+    desc: ""
+  },
+  {
+    name: "Spittleshrub Root",
+    amount: 7,
+    img: "../../avatar/block_oc.png",
+    desc: ""
+  },
+  {
+    name: "Edusan Whisker",
+    amount: 6,
+    img: "../../avatar/block_oc.png",
+    desc: ""
+  },
+  {
+    name: "Swarmshocker Jelly",
+    amount: 7,
+    img: "../../avatar/block_oc.png",
+    desc: ""
+  },
+  {
+    name: "Tachikatana Scale",
+    amount: 7,
+    img: "../../avatar/block_oc.png",
+    desc: ""
+  },
+  {
+    name: "Tachitakana Claw",
+    amount: 6,
+    img: "../../avatar/block_oc.png",
+    desc: ""
+  },
+  {
+    name: "Shiratsubi Wing",
+    amount: 10,
+    img: "../../avatar/block_oc.png",
+    desc: ""
+  },
+  {
+    name: "Spikewalker Meat",
+    amount: 10,
+    img: "../../avatar/block_oc.png",
+    desc: ""
+  },
+];
