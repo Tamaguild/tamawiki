8759d4c914340f635d61a7b08de047261e1a8ac1
Duong109 <thuy1984ktcn@gmail.com>
2026-05-10 10:07:53 +0700

    add skeleton guide page

---
+++ guide/skeleton/data.js
@@ -0,0 +1,77 @@
+export const DATA = [
+  {
+    name: "Layer 1",
+    description: `
+      <span class="header">PRAYING SKELETON SPREAD RADIUS & SPAWN CAP</span>
+      <p>
+      L1S0: <br>
+      - Spread Radius: 55 <br>
+      - Spawn Cap: 15
+      <br><br>
+      L1S1: <br>
+      - Spread Radius: 55 <br>
+      - Spawn Cap: 30
+      <br><br>
+      L1S2: <br>
+      - Spread Radius: 55 <br>
+      - Spawn Cap: 30
+      <br><br>
+      L1S3: <br>
+      - Spread Radius: 55 <br>
+      - Spawn Cap: 15
+      <br><br>
+      </p>
+      <span class="header">PRAYING SKELETON SPAWN RANGE</span>
+      <p>
+      L1S0: 
+      <br> 
+      - Y -20 to -220
+      <br> <br>
+      L1S1:
+      <br>
+      - Y 220 to -220
+      <br> <br>
+      L1S2:
+      <br>
+      - Y 220 to -220
+      <br> <br>
+      L1S3:
+      <br>
+      - Y 220 to 50
+      <br> <br>
+      </p>
+
+      <span class="header">LOOT TABLE</span>
+      <p>
+      <span class="header">Non Relic Drops</span>
+      <br>
+      Bundle: 1/10 <br>    
+      XP-Bottle: 1-2 1/100 <br>
+      Charcoal Sand: 1/25 <br>
+      Damage Sphere: 1/10 <br>
+
+      <span class="header">Relic Drops</span>
+      <br>
+      G1 Capillary Growth: 1/500 <br>
+      G1 Shifting Pike (Procoscis): 1/500 <br>
+      G2 Gold-Mending Wedge (Verdau Effax): 1/100 <br>
+      G3 Wanabushi (Ivy Call): 1/50 <br>
+      G3 Ray-Healing Plate: 1/50 <br>
+      G3 Petrifying Stone: 1/50 <br>
+      G4 Cradle of Fertility: 1/40 <br>
+      G4 Rock Top: 1/40 <br>
+      G4 Nil Spout: 1/40 <br>
+      G4 Sudden Pinch (Fright Instinct): 1/40 <br>
+      </p>
+    `,
+    grade: "Details for praying skeleton",
+    extra: `
+      <div class="tip">
+        <img class="tip-avatar" src="../../avatar/question_pigeon.png" alt="Tip avatar"> 
+        <div class="tip-box">
+          Relic here is decent. Recommend farm for Shifting Pike ( quite good one i guess? ) :mogicrazy:.
+        </div>
+      </div>
+    `       
+  },
+];
