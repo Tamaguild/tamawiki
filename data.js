60e894f2fde7b0480808ab9c78dbd56a8ef829d2
PluhGirl38 <pluhgirl38@gmail.com>
2026-05-27 18:24:10 +0200

    official release

--- data.js
+++ data.js
@@ -1,4 +1,5 @@
 export const CHANGE_LOG = [
+    { date: "2026-05-27", text: "official release" },
     { date: "2026-03-31", text: "First Update ever :D" },
     { date: "", text: "Add Mob Drop section, Relic section, Bestiary section, Map section,"}
 ];
