e049724b3ee1ea8ec95d8fc12d2083ea77024fdc
Duong109 <thuy1984ktcn@gmail.com>
2026-05-28 08:39:51 +0700

    add contact info :mogi:

--- data.js
+++ data.js
@@ -1,5 +1,6 @@
 export const CHANGE_LOG = [
-    { date: "2026-05-27", text: "official release" },
-    { date: "2026-03-31", text: "First Update ever :D" },
-    { date: "", text: "Add Mob Drop section, Relic section, Bestiary section, Map section,"}
+    { date: "2026-05-27", text: "Official Release" },
+    { date: "", text: "Add Bestiary, Relic Section, Mob Drop Section ( still W.I.P )"},
+    { date: "", text: "Add Miscellaneous Section ( still W.I.P ) along with an Orth Coin Calculator" },
+    { date: "", text: "Add Map Section ( heavily W.I.P ) and Gallery Section ( havent had any functional mechanism yet )" },
 ];
