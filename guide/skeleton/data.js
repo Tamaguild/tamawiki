f8c4b6492b5b7b2137b82806bb65ae8d2b28a3f5
PluhGirl38 <pluhgirl38@gmail.com>
2026-07-07 23:11:11 +0200

    added regular playerheads to playerheads page, added playerheads to skeleton page, linked them and fixed mistakes for ore and sherd drops

--- guide/skeleton/data.js
+++ guide/skeleton/data.js
@@ -60,6 +60,21 @@ export const DATA = [
       Miner Pottery Sherd: 1% or 1/100 <br>
       Explorer Pottery Sherd: 1% or 1/100 <br>
       Blade Pottery Sherd: 1% or 1/100 <br>
+      <span class="header">Playerheads</span> <br>
+      <a href="../playerhead/playerhead.html?page=20">Silkfang Egg Sac</a>: 1% or 1/100 <br>
+      <a href="../playerhead/playerhead.html?page=14">Old Orth Bell</a>: 2% or 1/50 <br>
+      <a href="../playerhead/playerhead.html?page=18">Praying Skeleton Skull</a>: 3% or 1/33.33 <br>
+      <a href="../playerhead/playerhead.html?page=23">Worn Out Lantern</a>: 0.17% or 1/588.24 <br>
+      <a href="../playerhead/playerhead.html?page=15">Old Pot</a>: 0.17% or 1/588.24 <br>
+      <a href="../playerhead/playerhead.html?page=5">Cauldron of Bones</a>: 0.17% or 1/588.24 <br>
+      <a href="../playerhead/playerhead.html?page=22">White Bag</a>: 0.17% or 1/588.24 <br>
+      <a href="../playerhead/playerhead.html?page=3">Brown Bag</a>: 0.17% or 1/588.24 <br>
+      <a href="../playerhead/playerhead.html?page=2">Barrel</a>: 0.17% or 1/588.24 <br>
+      <a href="../playerhead/playerhead.html?page=4">Brown Chest</a>: 0.17% or 1/588.24 <br>
+      <a href="../playerhead/playerhead.html?page=6">Cooking Pot</a>: 0.17% or 1/588.24 <br>
+      <a href="../playerhead/playerhead.html?page=16">Old Rope</a>: 0.17% or 1/588.24 <br>
+      <a href="../playerhead/playerhead.html?page=21">Used Rope</a>: 0.17% or 1/588.24 <br>
+      <a href="../playerhead/playerhead.html?page=1">Bag of Coins</a>: 0.17% or 1/588.24 <br>
       <span class="header">Relic Drops</span> <br>
       G1 <a href="../../relics/relic.html?page=34">Capillary Growth</a> 0.2% or 1/500 <br>
       G1 <a href="../../relics/relic.html?page=33">Shifting Pike</a> 0.2% or 1/500 <br>
@@ -105,6 +120,21 @@ export const DATA = [
       Miner Pottery Sherd: 1% or 1/100 <br>
       Explorer Pottery Sherd: 1% or 1/100 <br>
       Blade Pottery Sherd: 1% or 1/100 <br>
+      <span class="header">Playerheads</span> <br>
+      <a href="../playerhead/playerhead.html?page=13">Man-toyer Skull</a>: 1% or 1/100 <br>
+      <a href="../playerhead/playerhead.html?page=9">Eternal Shine</a>: 2% or 1/50 <br>
+      <a href="../playerhead/playerhead.html?page=18">Praying Skeleton Skull</a>: 3% or 1/33.33 <br>
+      <a href="../playerhead/playerhead.html?page=23">Worn Out Lantern</a>: 0.17% or 1/588.24 <br>
+      <a href="../playerhead/playerhead.html?page=15">Old Pot</a>: 0.17% or 1/588.24 <br>
+      <a href="../playerhead/playerhead.html?page=5">Cauldron of Bones</a>: 0.17% or 1/588.24 <br>
+      <a href="../playerhead/playerhead.html?page=22">White Bag</a>: 0.17% or 1/588.24 <br>
+      <a href="../playerhead/playerhead.html?page=3">Brown Bag</a>: 0.17% or 1/588.24 <br>
+      <a href="../playerhead/playerhead.html?page=2">Barrel</a>: 0.17% or 1/588.24 <br>
+      <a href="../playerhead/playerhead.html?page=4">Brown Chest</a>: 0.17% or 1/588.24 <br>
+      <a href="../playerhead/playerhead.html?page=6">Cooking Pot</a>: 0.17% or 1/588.24 <br>
+      <a href="../playerhead/playerhead.html?page=16">Old Rope</a>: 0.17% or 1/588.24 <br>
+      <a href="../playerhead/playerhead.html?page=21">Used Rope</a>: 0.17% or 1/588.24 <br>
+      <a href="../playerhead/playerhead.html?page=1">Bag of Coins</a>: 0.17% or 1/588.24 <br>
       <span class="header">Relic Drops</span> <br>
       G1 <a href="../../relics/relic.html?page=35">Necrometallic Blade</a> 0.18% or 1/555.5 <br>
       G1 <a href="../../relics/relic.html?page=33">Shifting Pike</a> 0.05% or 1/2000 <br>
@@ -136,11 +166,27 @@ export const DATA = [
       Gold Ingot: 1-2 10% or 1/10 <br>
       Iron Ingot: 2-3 30% or 1/3.33 <br>
       <span class="header">Pottery Sherds</span> <br>
-      Skull Pottery Sherd: 1.4% or 1/ 71.4285 <br>
-      Archer Pottery Sherd: 1.4% or 1/ 71.4285 <br>
-      Miner Pottery Sherd: 1.4% or 1/ 71.4285 <br>
-      Explorer Pottery Sherd: 1.4% or 1/ 71.4285 <br>
-      Blade Pottery Sherd: 1.4% or 1/ 71.4285 <br>
+      Skull Pottery Sherd: 1.4% or 1/71.4285 <br>
+      Archer Pottery Sherd: 1.4% or 1/71.4285 <br>
+      Miner Pottery Sherd: 1.4% or 1/71.4285 <br>
+      Explorer Pottery Sherd: 1.4% or 1/71.4285 <br>
+      Blade Pottery Sherd: 1.4% or 1/71.4285 <br>
+      <span class="header">Playerheads</span> <br>
+      <a href="../playerhead/playerhead.html?page=17">Pink-shining Fruit</a>: 1% or 1/100 <br>
+      <a href="../playerhead/playerhead.html?page=19">Rotting Plant</a>: 1% or 1/100 <br>
+      <a href="../playerhead/playerhead.html?page=9">Eternal Shine</a>: 1% or 1/100 <br>
+      <a href="../playerhead/playerhead.html?page=8">Delver Corpse Skull</a>: 3% or 1/33.33 <br>
+      <a href="../playerhead/playerhead.html?page=23">Worn Out Lantern</a>: 0.3% or 1/333.33 <br>
+      <a href="../playerhead/playerhead.html?page=15">Old Pot</a>: 0.3% or 1/333.33 <br>
+      <a href="../playerhead/playerhead.html?page=5">Cauldron of Bones</a>: 0.3% or 1/333.33 <br>
+      <a href="../playerhead/playerhead.html?page=22">White Bag</a>: 0.3% or 1/333.33 <br>
+      <a href="../playerhead/playerhead.html?page=3">Brown Bag</a>: 0.3% or 1/333.33 <br>
+      <a href="../playerhead/playerhead.html?page=2">Barrel</a>: 0.3% or 1/333.33 <br>
+      <a href="../playerhead/playerhead.html?page=4">Brown Chest</a>: 0.3% or 1/333.33 <br>
+      <a href="../playerhead/playerhead.html?page=6">Cooking Pot</a>: 0.3% or 1/333.33 <br>
+      <a href="../playerhead/playerhead.html?page=16">Old Rope</a>: 0.3% or 1/333.33 <br>
+      <a href="../playerhead/playerhead.html?page=21">Used Rope</a>: 0.3% or 1/333.33 <br>
+      <a href="../playerhead/playerhead.html?page=1">Bag of Coins</a>: 0.3% or 1/333.33 <br>
       <span class="header">Relic Drops</span> <br>
       G1 <a href="../../relics/relic.html?page=37">Atlas Dignitas</a> 0.1% or 1/1000 <br>
       G1 <a href="../../relics/relic.html?page=38">Crescent Hatchet</a> 0.1% or 1/1000 <br>
@@ -168,13 +214,29 @@ export const DATA = [
       <span class="header">Ores</span> <br>
       Diamond: 1-2 15% or 1/6.67 <br>
       Gold Ingot: 1-2 45% or 1/2.22 <br>
-      Iron Ingot: 2-4 50% or 1/20 <br>
+      Iron Ingot: 2-4 50% or 1/2 <br>
       <span class="header">Pottery Sherds</span> <br>
       Skull Pottery Sherd: 2.5% or 1/40 <br>
       Archer Pottery Sherd: 2.5% or 1/40 <br>
       Miner Pottery Sherd: 2.5% or 1/40 <br>
       Explorer Pottery Sherd: 2.5% or 1/40 <br>
       Blade Pottery Sherd: 2.5% or 1/40 <br>
+      <span class="header">Playerheads</span> <br>
+      <a href="../playerhead/playerhead.html?page=7">Cracked Giant Hammerbeak Egg</a>: 2% or 1/50 <br>
+      <a href="../playerhead/playerhead.html?page=10">Fossilized Relic</a>: 2% or 1/50 <br>
+      <a href="../playerhead/playerhead.html?page=9">Eternal Shine</a>: 2% or 1/50 <br>
+      <a href="../playerhead/playerhead.html?page=8">Delver Corpse Skull</a>: 6% or 1/16.67 <br>
+      <a href="../playerhead/playerhead.html?page=23">Worn Out Lantern</a>: 1% or 1/100 <br>
+      <a href="../playerhead/playerhead.html?page=15">Old Pot</a>: 1% or 1/100 <br>
+      <a href="../playerhead/playerhead.html?page=5">Cauldron of Bones</a>: 1% or 1/100 <br>
+      <a href="../playerhead/playerhead.html?page=22">White Bag</a>: 1% or 1/100 <br>
+      <a href="../playerhead/playerhead.html?page=3">Brown Bag</a>: 1% or 1/100 <br>
+      <a href="../playerhead/playerhead.html?page=2">Barrel</a>: 1% or 1/100 <br>
+      <a href="../playerhead/playerhead.html?page=4">Brown Chest</a>: 1% or 1/100 <br>
+      <a href="../playerhead/playerhead.html?page=6">Cooking Pot</a>: 1% or 1/100 <br>
+      <a href="../playerhead/playerhead.html?page=16">Old Rope</a>: 1% or 1/100 <br>
+      <a href="../playerhead/playerhead.html?page=21">Used Rope</a>: 1% or 1/100 <br>
+      <a href="../playerhead/playerhead.html?page=1">Bag of Coins</a>: 1% or 1/100 <br>
       <span class="header">Relic Drops</span> <br>
       G1 <a href="../../relics/relic.html?page=40">Blaze Reap</a> 0.1% or 1/1000 <br>
       G1 <a href="../../relics/relic.html?page=41">Reverberating Edge</a> 0.2% or 1/500 <br>
@@ -209,6 +271,21 @@ export const DATA = [
       Miner Pottery Sherd: 2.8% or 1/35.714285 <br>
       Explorer Pottery Sherd: 2.8% or 1/35.714285 <br>
       Blade Pottery Sherd: 2.8% or 1/35.714285 <br>
+      <span class="header">Playerheads</span> <br>
+      <a href="../playerhead/playerhead.html?page=11">Frozen Gem</a>: 2% or 1/50 <br>
+      <a href="../playerhead/playerhead.html?page=12">Frozen Relic</a>: 2% or 1/50 <br>
+      <a href="../playerhead/playerhead.html?page=8">Delver Corpse Skull</a>: 6% or 1/16.67 <br>
+      <a href="../playerhead/playerhead.html?page=23">Worn Out Lantern</a>: 1% or 1/100 <br>
+      <a href="../playerhead/playerhead.html?page=15">Old Pot</a>: 1% or 1/100 <br>
+      <a href="../playerhead/playerhead.html?page=5">Cauldron of Bones</a>: 1% or 1/100 <br>
+      <a href="../playerhead/playerhead.html?page=22">White Bag</a>: 1% or 1/100 <br>
+      <a href="../playerhead/playerhead.html?page=3">Brown Bag</a>: 1% or 1/100 <br>
+      <a href="../playerhead/playerhead.html?page=2">Barrel</a>: 1% or 1/100 <br>
+      <a href="../playerhead/playerhead.html?page=4">Brown Chest</a>: 1% or 1/100 <br>
+      <a href="../playerhead/playerhead.html?page=6">Cooking Pot</a>: 1% or 1/100 <br>
+      <a href="../playerhead/playerhead.html?page=16">Old Rope</a>: 1% or 1/100 <br>
+      <a href="../playerhead/playerhead.html?page=21">Used Rope</a>: 1% or 1/100 <br>
+      <a href="../playerhead/playerhead.html?page=1">Bag of Coins</a>: 1% or 1/100 <br>
       <span class="header">Relic Drops</span> <br>
       G1 <a href="../../relics/relic.html?page=43">Yūkonokage</a> 0.13% or 1/770 <br>
       G1 <a href="../../relics/relic.html?page=44">Subtle Pull</a> 0.13% or 1/770 <br>
