fd1a0c9a6df4f49f4775481ab57ea3fad50df5a6
Bortikr23 <bortikr23@gmail.com>
2026-09-20 17:11:04 -0700

    Backpacks and Hats

--- cosmetics/data.js
+++ cosmetics/data.js
@@ -10,36 +10,40 @@ export const DATA = {
       price: 250,
 
       scale: 1.5,
-      position: { x: -0.75, y: 1.25, z: -1 },
-      rotation: { x: 0, y: 0, z: 0 }
+      position: { x: -0.66, y: 1.3, z: -1 },
+      rotation: { x: 0.1, y: -0.1, z: 0 }
     },
     {
       name: "Cool Hat",
       model: './models/feather_hat_black.glb',
+      gif: './GIFS/feather_hat_black.gif',
 
-      scale: 1,
-      position: { x: -0.5, y: 1.9, z: -0.45 },
-      rotation: { x: 0, y: 0, z: 0 }
+      scale: 0.9,
+      position: { x: -0.4, y: 1.95, z: -0.45 },
+      rotation: { x: 0.1, y: -0.1, z: 0 }
     },
     {
       name: "Agile Hat (Black)",
       model: './models/agile_hat_black.glb',
+      gif: './GIFS/agile_hat_black.gif',
 
       scale: 1,
-      position: { x: -0.5, y: 1.9, z: -0.45 },
-      rotation: { x: 0.1, y: 0, z: 0 }
+      position: { x: -0.45, y: 1.85, z: -0.525 },
+      rotation: { x: 0.1, y: -0.1, z: 0 }
     },
     {
       name: "Agile Hat (Green)",
       model: './models/agile_hat_green.glb',
+      gif: './GIFS/agile_hat_green.gif',
 
       scale: 1,
-      position: { x: -0.5, y: 1.9, z: -0.45 },
-      rotation: { x: 0.1, y: 0, z: 0 }
+      position: { x: -0.45, y: 1.85, z: -0.525 },
+      rotation: { x: 0.1, y: -0.1, z: 0 }
     }, 
     {
       name: "Artist Hat",
       model: './models/artist_hat.glb',
+      gif: './GIFS/artist_hat.gif',
 
       scale: 0.75,
       position: { x: -0.35, y: 1.9, z: -0.35 },
@@ -48,7 +52,8 @@ export const DATA = {
     {
       name: "Bondrewd Helmet",
       model: './models/bondrewd_helmet.glb',
-      
+      gif: './GIFS/bondrewd_helmet.gif',
+
       scale: 1.4,
       position: { x: -0.61, y: 1.15, z: -0.94 },
       rotation: { x: 0.1, y: -0.1, z: 0 }
@@ -56,6 +61,7 @@ export const DATA = {
     {
       name: "Cook Hat",
       model: './models/cook_hat.glb',
+      gif: './GIFS/cook_hat.gif',
 
       scale: 0.85,
       position: { x: -0.38, y: 1.8, z: -0.45 },
@@ -64,6 +70,7 @@ export const DATA = {
     { 
       name: "Detchuagan Hat",
       model: './models/detchuagan_hat.glb',
+      gif: './GIFS/detchuagan_hat.gif',
 
       scale: 0.9,
       position: { x: -0.4, y: 1.7, z: -0.45 },
@@ -72,6 +79,7 @@ export const DATA = {
     {
       name: "Dooby Helmet",
       model: './models/dooby_helmet.glb',
+      gif: './GIFS/dooby_helmet.gif',
 
       scale: 1,
       position: { x: -0.45, y: 1.8, z: -0.55 },
@@ -80,6 +88,7 @@ export const DATA = {
     {
       name: "Feather Hat (Blue)",
       model: './models/feahter_hat_blue.glb',
+      gif: './GIFS/feather_hat_blue.gif',
 
       scale: 1,
       position: { x: -0.45, y: 1.9, z: -0.5 },
@@ -88,6 +97,7 @@ export const DATA = {
     {
       name: "Feather Hat (Black)",
       model: './models/feather_hat_black.glb',
+      gif: './GIFS/feather_hat_black.gif',
 
       scale: 1,
       position: { x: -0.45, y: 1.9, z: -0.5 },
@@ -96,6 +106,7 @@ export const DATA = {
     {
       name: "Habo Hat",
       model: './models/habo_hat.glb',
+      gif: './GIFS/habo_hat.gif',
 
       scale: 0.8,
       position: { x: -0.35, y: 1.55, z: -0.55 },
@@ -104,6 +115,7 @@ export const DATA = {
     {
       name: "Henya Helmet",
       model: './models/henya_helmet.glb',
+      gif: './GIFS/henya_helmet.gif',
 
       scale: 1,
       position: { x: -0.45, y: 1.8, z: -0.5 },
@@ -112,41 +124,46 @@ export const DATA = {
     {
       name: "Hunter Hat",
       model: './models/hunter_hat.glb',
+      gif: './GIFS/hunter_hat.gif',
 
-      scale: 1,
-      position: { x: -0.45, y: 1.8, z: -0.5 },
+      scale: 0.95,
+      position: { x: -0.45, y: 1.5, z: -0.5 },
       rotation: { x: 0.1, y: -0.1, z: 0 }
     },
     {
       name: "Ocean Hat",
       model: './models/ocean_hat.glb',
+      gif: './GIFS/ocean_hat.gif',
 
-      scale: 1,
-      position: { x: -0.45, y: 1.8, z: -0.5 },
+      scale: 1.1,
+      position: { x: -0.5, y: 1.9, z: -0.55 },
       rotation: { x: 0.1, y: -0.1, z: 0 } 
     }, 
     {
       name: "Purple Bucket Hat",
       model: './models/purple_bucket_hat.glb',
+      gif: './GIFS/purple_bucket_hat.gif',
 
       scale: 1,
-      position: { x: -0.45, y: 1.8, z: -0.5 },
+      position: { x: -0.45, y: 1.5, z: -0.55 },
       rotation: { x: 0.1, y: -0.1, z: 0 }
     },
     {
       name: "Reg Helmet",
       model: './models/reg_helmet.glb',
+      gif: './GIFS/reg_helmet.gif',
 
-      scale: 1,
-      position: { x: -0.45, y: 1.8, z: -0.5 },
+      scale: 2,
+      position: { x: -0.82, y: 1.1, z: -1.2 },
       rotation: { x: 0.1, y: -0.1, z: 0 }
     },
     {
       name: "Traveler Hat 2",
       model: './models/traveler_hat_2.glb',
+      gif: './GIFS/traveler_hat_2.gif',
 
       scale: 1,
-      position: { x: -0.45, y: 1.8, z: -0.5 },
+      position: { x: -0.45, y: 1.6, z: -0.55 },
       rotation: { x: 0.1, y: -0.1, z: 0 }
     }
   
@@ -156,18 +173,48 @@ export const DATA = {
     {
       name: "Default Backpack",
       model: './models/default_backpack.glb',
+      gif: './GIFS/default_backpack.gif',
       price: 300,
 
-      scale: 1.5,
-      position: { x: 0.75, y: 0.75, z: 1.1 },
+      scale: 1.8,
+      position: { x: 0.9, y: 0.8, z: 1.2 },
       rotation: { x: 0, y: Math.PI, z: 0 }
     },
     {
       name: "Big Bag",
       model: './models/satchel.glb',
+      gif: './GIFS/satchel.gif',
       scale: 1.8,
-      position: { x: 0.7, y: 0.6, z: 1.2 },
+      position: { x: 0.9, y: 0.9, z: 1.2 },
+      rotation: { x: 0, y: Math.PI, z: 0 }
+    },
+    {
+      name: "Bondrewd Backpack",
+      model: './models/bondrewd_backpack.glb',
+      gif: './GIFS/bondrewd_backpack.gif',
+
+      scale: 1.3,
+      position: { x: 0.65, y: 2.1, z: 0.95 },
+      rotation: { x: 0, y: Math.PI, z: 0 }
+    },
+    {
+      name: "Cauldron Backpack",
+      model: './models/cauldron_backpack.glb',
+      gif: './GIFS/cauldron_backpack.gif',
+
+      scale: 1.2,
+      position: { x: 0.6, y: 0.8, z: 1.05 },
+      rotation: { x: 0, y: Math.PI, z: 0 }
+    },
+    {
+      name: "Nerhtantan Backpack",
+      model: './models/neritantan_backpack.glb',
+      gif: './GIFS/neritantan_backpack.gif',
+
+      scale: 1.5,
+      position: { x: 0.75, y: 0.25, z: 1.05 },
       rotation: { x: 0, y: Math.PI, z: 0 }
     }
+
   ]
 };
